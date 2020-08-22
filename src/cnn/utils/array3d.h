73ff5b04178c102313f015dab37fc7514cffeef2
Piotrekszmel <piotr.szmelczynski@gmail.com>
2020-08-22 13:37:27 +0200

    create array3d class with slide3d class inside it

---
+++ src/cnn/utils/array3d.h
@@ -0,0 +1,8 @@
+//
+// Created by stanislaw on 22.08.2020.
+//
+
+#ifndef CNN_ARRAY3D_H
+#define CNN_ARRAY3D_H
+
+#endif //CNN_ARRAY3D_H
---
+++ src/cnn/utils/log.h
@@ -0,0 +1,8 @@
+//
+// Created by stanislaw on 22.08.2020.
+//
+
+#ifndef CNN_LOG_H
+#define CNN_LOG_H
+
+#endif //CNN_LOG_H
